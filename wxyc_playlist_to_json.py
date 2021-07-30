afaa092a13e0f3511b97d7d85c60356038b04031
robotpsychology <42260976+robotpsychology@users.noreply.github.com>
2021-07-30 18:29:35 -0400

    Lots of updates. Seperated subfunctions of wxyc_playlist_parser in order to handle both the archive and live playlists. Each function formats the music_dict according to the structure of the page and that can then be used in the playlist_json_dump function. Modified music_dict to recognize requests and rotation tracks correctly. Will write a function for the wxyc search results page next.

--- wxyc_playlist_to_json.py
+++ wxyc_playlist_to_json.py
@@ -4,6 +4,8 @@ import json
 from datetime import datetime
 from bs4 import BeautifulSoup
 
+# THE LIVE PLAYLSIST PAGE AND ARCHIVE PAGES ARE FORMATTED DIFFERENTLY. CREATE A FUNCTION FOR EACH, THEN A MAIN FUNCTION THAT DETERMINES WHICH TYPE OF PAGE IT IS AND RUNS THE APPROPRIATE FUNCTION.
+
 def clean_file_name(sourcestring,  removestring="%:/,.\\[]<>*?"):
     """Taken and slightly modified from https://www.programcreek.com/python/?CodeExample=clean+filename"""
     
@@ -26,7 +28,9 @@ def clean_file_name(sourcestring,  removestring="%:/,.\\[]<>*?"):
     #remove the undesireable characters
     return ''.join([c for c in sourcestring if c not in removestring])
 
-def wxyc_playlist_parser(url, playlist_name=f"wxyc_live_playlist {datetime.now().strftime('%m-%d-%Y_%H-%M-%S-%p')}"):
+
+
+def wxyc_playlist_parser(url, playlist_name):
     """
     Argument 1: WXYC Playlist URL
     Argument 2 (optional): Input desired JSON filename if you are parsing a playlist that is not the current live playlist. 
@@ -39,7 +43,6 @@ def wxyc_playlist_parser(url, playlist_name=f"wxyc_live_playlist {datetime.now()
 
     parsed_content = []
     dict_formatted_content = []
-    music_dict = {}
 
     for rows in tr:
         parsed_content.append(rows.contents)
@@ -51,44 +54,138 @@ def wxyc_playlist_parser(url, playlist_name=f"wxyc_live_playlist {datetime.now()
         
         dict_formatted_content.append([item.string for item in lst])
     
-    # print(parsed_content)
+    # print(parsed_content[1], parsed_content[18])
     # print(dict_formatted_content)
 
+
+    if '/playlists/recent.html' in url:
+        return dict_formatted_content, 'live', url, playlist_name
+    elif 'playlists/radioShow?radioShowID' in url:
+        return dict_formatted_content, 'archive', url, playlist_name
+    elif 'playlists/searchPlaylists' in url:
+        return dict_formatted_content, 'search', url, playlist_name
+    else:
+        print('Invalid URL')
+        return None
+
+
+
+def wxyc_live_playlist_dict(dict_formatted_content):
     counter = 0
+    music_dict = {}
+
     for lst in dict_formatted_content:
         music_dict[counter] = {
-            'request': None,
-            'artists': None,
+            'rotation': None,
+            'artist': None,
             'song': None,
-            'album': None,
-            'label': None
+            'release': None,
+            'label': None,
+            'request': None,
         }
+
         if lst[0] == '*':
+            music_dict[counter]['rotation'] = True
+        else:
+            music_dict[counter]['rotation'] = False
+
+        if lst[-1] == '*':
             music_dict[counter]['request'] = True
         else:
             music_dict[counter]['request'] = False
 
-        music_dict[counter]['artists'] = lst[1]
+        music_dict[counter]['artist'] = lst[1]
         music_dict[counter]['song'] = lst[2]
-        music_dict[counter]['album'] = lst[3]
+        music_dict[counter]['release'] = lst[3]
         music_dict[counter]['label'] = lst[4]
 
         counter += 1
 
+    return music_dict
 
+
+def wxyc_archive_playlist_dict(dict_formatted_content):
+    counter = 0
+    music_dict = {}
+
+    for lst in dict_formatted_content:
+        music_dict[counter] = {
+            'rotation': None,
+            'request': None,
+            'artist': None,
+            'song': None,
+            'release': None,
+            'label': None
+        }
+
+        if lst[0] == '*':
+            music_dict[counter]['rotation'] = True
+        else:
+            music_dict[counter]['rotation'] = False
+
+        if lst[1] == '*':
+            music_dict[counter]['request'] = True
+        else:
+            music_dict[counter]['request'] = False
+
+        music_dict[counter]['artist'] = lst[1]
+        music_dict[counter]['song'] = lst[2]
+        music_dict[counter]['release'] = lst[3]
+        music_dict[counter]['label'] = lst[4]
+
+        counter += 1
+
+    return music_dict
+
+# def wxyc_search_playlist_dict():
+
+
+def playlist_json_dump(music_dict, playlist_name):
     # Make sure to look at first run file to see an error we need to catch. The use of odd symbols such as A with tilde and copyright symbol.
     with open(clean_file_name(f"{playlist_name}") + '.json', "w") as outfile:
         json.dump(music_dict, outfile, indent=4)
 
-    return music_dict
+
+
+def main(url, playlist_name=f"wxyc_live_playlist {datetime.now().strftime('%m-%d-%Y_%H-%M-%S-%p')}"):
+    parser_result = wxyc_playlist_parser(url, playlist_name)
+    if parser_result == None:
+        print('Something went wrong. Probably a bad URL.')
+        return parser_result
+    
+    dict_formatted_content = parser_result[0]
+    playlist_type = parser_result[1]
+    playlist_url = parser_result[2]
+    cleaned_playlist_name = parser_result[3]
+    music_dict = {}
+
+    if playlist_type == 'live':
+        music_dict = wxyc_live_playlist_dict(dict_formatted_content)
+
+        playlist_json_dump(music_dict, cleaned_playlist_name)
+    elif playlist_type == 'archive':
+        music_dict = wxyc_archive_playlist_dict(dict_formatted_content)
+
+        if 'wxyc_live_playlist' in cleaned_playlist_name:
+            playlist_json_dump(music_dict, f"Unnamed Archive Playlist - Downloaded {datetime.now().strftime('%m-%d-%Y_')}")
+        else:
+            playlist_json_dump(music_dict, cleaned_playlist_name)
+    else:
+        pass
+        "elif for search playlist goes here"
+    
+
 
 
     
 # Live Playlist example:
-wxyc_playlist_parser('http://www.wxyc.info/playlists/recent.html')
+main('http://www.wxyc.info/playlists/recent.html')
 
-# Archived Playlist: 
-wxyc_playlist_parser('http://wxyc.info/playlists/radioShow?radioShowID=156207', 'DJ Appa 7/18/21 3AM-6AM')
+# Archive Playlist (Forbidden Characters): 
+main('http://wxyc.info/playlists/radioShow?radioShowID=156207', 'DJ Appa 7/18/21 3AM-6AM')
+
+# Archive Playlist (Left Unnamed): 
+main('http://wxyc.info/playlists/radioShow?radioShowID=156207')
 
 
 
