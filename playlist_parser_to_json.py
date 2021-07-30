f8cd8568745265dcca8e6627b12d7addbc8777c6
robotpsychology <42260976+robotpsychology@users.noreply.github.com>
2021-07-30 03:04:13 -0400

    Updated parser function to include an optional argument for changing the JSON file name, in the case of sing the parser for an older archived playlist. The JSON file is now saved with the current date and time.  Added a filename cleaning function from a random website. If a custom filename is inputted, it will be cleaned of forbidden characters. '.json' is now added after on the f-string for the file write because of this cleaning.

--- playlist_parser_to_json.py
+++ playlist_parser_to_json.py
@@ -1,9 +1,37 @@
 import os
 import requests
 import json
+from datetime import datetime
 from bs4 import BeautifulSoup
 
-def wxyc_playlist_parser(url):
+def clean_file_name(sourcestring,  removestring="%:/,.\\[]<>*?"):
+    """Taken and slightly modified from https://www.programcreek.com/python/?CodeExample=clean+filename"""
+    
+    """Clean a string by removing selected characters.
+
+    Creates a legal and 'clean' source string from a string by removing some 
+    clutter and  characters not allowed in filenames.
+    A default set is given but the user can override the default string.
+
+    Args:
+        | sourcestring (string): the string to be cleaned.
+        | removestring (string): remove all these characters from the string (optional).
+
+    Returns:
+        | (string): A cleaned-up string.
+
+    Raises:
+        | No exception is raised.
+    """
+    #remove the undesireable characters
+    return ''.join([c for c in sourcestring if c not in removestring])
+
+def wxyc_playlist_parser(url, playlist_name=f"wxyc_live_playlist {datetime.now().strftime('%m-%d-%Y_%H-%M-%S-%p')}"):
+    """
+    Argument 1: WXYC Playlist URL
+    Argument 2 (optional): Input desired JSON filename if you are parsing a playlist that is not the current live playlist. 
+        Will automatically include current date and time. If custom name is inputted, it will be cleaned of potential forbidden file characters.
+    """
     html_doc = requests.get(url)
     soup = BeautifulSoup(html_doc.text, 'html.parser')
 
@@ -49,21 +77,21 @@ def wxyc_playlist_parser(url):
 
 
     # Make sure to look at first run file to see an error we need to catch. The use of odd symbols such as A with tilde and copyright symbol.
-    with open("live_playlist.json", "w") as outfile:
+    with open(clean_file_name(f"{playlist_name}") + '.json', "w") as outfile:
         json.dump(music_dict, outfile, indent=4)
 
     return music_dict
 
 
     
-
-
-
-
-            
-
-
+# Live Playlist example:
 wxyc_playlist_parser('http://www.wxyc.info/playlists/recent.html')
+
+# Archived Playlist: 
+wxyc_playlist_parser('http://wxyc.info/playlists/radioShow?radioShowID=156207', 'DJ Appa 7/18/21 3AM-6AM')
+
+
+
 
 
 
